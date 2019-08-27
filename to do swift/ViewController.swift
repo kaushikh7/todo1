13a2e82a3c5ef911ea9a9fc18d3910e527fefe63
01hw359200 <01hw359200@01hw359200.local>
2019-08-27 11:51:04 +0530

    Initial Commit

---
+++ to do swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  to do swift
+//
+//  Created by 01hw359200 on 05/06/1941 Saka.
+//  Copyright © 1941 01hw359200. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
